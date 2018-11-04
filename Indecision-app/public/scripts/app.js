95f694e1c2b7d75624720df980e45c6485e712c1
MichaeliG <michaeligade@gmail.com>
2018-11-03 20:19:32 -0400

    Event handleers

--- Indecision-app/public/scripts/app.js
+++ Indecision-app/public/scripts/app.js
@@ -32,21 +32,48 @@ function getLocation(location) {
         );
     }
 }
-var template2 = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        user.name ? user.name : 'Anonymous'
-    ),
-    user.age && user.age >= 18 && React.createElement(
-        'p',
-        null,
-        'Age: ',
-        user.age
-    ),
-    getLocation(user.location)
-);
+var count = 0;
+var addOne = function addOne() {
+    count++;
+    renderCounterApp();
+};
+var minusOne = function minusOne() {
+    count--;
+    renderCounterApp();
+};
+var reset = function reset() {
+    count = 0;
+    renderCounterApp();
+};
+
 var appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+
+var renderCounterApp = function renderCounterApp() {
+    var template2 = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            'Count: ',
+            count
+        ),
+        React.createElement(
+            'button',
+            { onClick: addOne },
+            ' +1'
+        ),
+        React.createElement(
+            'button',
+            { onClick: minusOne },
+            ' -1'
+        ),
+        React.createElement(
+            'button',
+            { onClick: reset },
+            ' reset'
+        )
+    );
+    ReactDOM.render(template2, appRoot);
+};
+renderCounterApp();
